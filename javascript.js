bf111e76ac03bd23ddaeb9b9300ade811f825abd
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-11 12:08:08 -0500

    feat: Add basic arithmetic functions

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// Basic arithmetic functions
+function add(a, b) { return a + b };
+function subtract(a, b) { return a - b };
+function multiply(a, b) { return a * b };
+function divide(a, b) { return a / b };
+
+function operate(operand, a, b) {
+    switch (operand) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            throw SyntaxError(`Invalid operand: ${operand}`);
+    }
+}
+
+let numberA = 0;
+let numberB = 0;
+let operand;
