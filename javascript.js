121a9d2f3bf30b1a031552f57f9c1cdd292f6684
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-11 16:09:37 -0500

    feat: Add arithmetic functions to GUI
    Multiplication, division, arithmetic, and subtraction with two numbers
    works via clicking the digits and operands.
    
    Bug: Trying to press another operand after that first operation returns
    NaN.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+// Shared elements
+const displayText = document.getElementById("display");
+
+// Shared variables
+let numberA = "";
+let numberB = "";
+let operand = "";
+
 // Basic arithmetic functions
 function add(a, b) { return a + b };
 function subtract(a, b) { return a - b };
@@ -5,20 +13,67 @@ function multiply(a, b) { return a * b };
 function divide(a, b) { return a / b };
 
 function operate(operand, a, b) {
+    let total = 0;
+    a = parseInt(a);
+    b = parseInt(b);
+
+    console.log(`A: ${a}`);
+    console.log(`B: ${b}`);
     switch (operand) {
-        case "+":
-            return add(a, b);
-        case "-":
-            return subtract(a, b);
-        case "*":
-            return multiply(a, b);
-        case "/":
-            return divide(a, b);
+        case "add":
+            total = add(a, b);
+            break;
+        case "subtract":
+            total = subtract(a, b);
+            break;
+        case "multiply":
+            total = multiply(a, b);
+            break;
+        case "divide":
+            total = divide(a, b);
+            break;
         default:
             throw SyntaxError(`Invalid operand: ${operand}`);
     }
+
+    console.log(total);
+    numberA = total;
+    numberB = "";
+    if (operand)
+        displayText.textContent = numberA;
 }
 
-let numberA = 0;
-let numberB = 0;
-let operand;
+function checkDisplayOverflow() {
+    return displayText.textContent.length >= 12;
+}
+
+function display(symbol) {
+    displayText.textContent += symbol;
+}
+
+function displayDigit() {
+    if (checkDisplayOverflow()) return;
+
+    symbol = this.textContent;
+    display(symbol);
+    (operand === "") ? numberA += symbol : numberB += symbol;
+}
+
+const digits = document.querySelectorAll(".digit");
+digits.forEach(function (digit) {
+    digit.addEventListener("click", displayDigit);
+});
+
+function displayOperand() {
+    if (checkDisplayOverflow()) return;
+
+    symbol = this.textContent;
+    console.log(symbol);
+    display(this.textContent);
+    (operand === "") ? operand = this.id : operate(operand, numberA, numberB);
+}
+
+const operands = document.querySelectorAll(".operand");
+operands.forEach(function (operand) {
+    operand.addEventListener("click", displayOperand);
+});
